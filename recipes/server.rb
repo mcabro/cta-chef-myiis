74cb12e8f99dba2731920a2bd7b369b656f29da2
McAbro <mcabro@example.com>
2019-06-18 19:23:17 +0000

    Añadida información del nodo en la página web. Cambio de versión a la 0.2.0.

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,7 +9,12 @@ powershell_script 'Install IIS' do
 end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!(chef style)'
+  content "<h1>Hello, world!(chef style)</h1>
+  <h2> Platform: #{node['platform']} </h2>
+  <h2> Hostname: #{node['hostname']} </h2>
+  <h2> Memory: #{node['memory']['total']} </h2>
+  <h2> CPU MHz: #{node['cpu']['0']['mhz']} </h2>
+  "
 end
 
 service 'w3svc' do
