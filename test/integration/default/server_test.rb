fa90615a3a20ae68200bfeae3c83f33397968e68
McAbro <mcabro@example.com>
2019-06-18 18:34:21 +0000

    Added some tests.

--- test/integration/default/server_test.rb
+++ test/integration/default/server_test.rb
@@ -13,6 +13,26 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+
+
+######## TEST CONFIGURATION ########
+
+describe service('w3svc') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+######## TEST PROMISE ########
+describe port(80) do
+  it { should be_listening }
+end
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+
+describe http('localhost') do
+  its('status') { should eq 200 }
+  its('headers.Content-Type') { should include 'text/html' }
+  its('body') { should include 'Hello, world!(chef style)' }
 end
