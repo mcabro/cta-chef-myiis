8d5d96975935bf8416c370f2a9a6bb38fc944202
McAbro <mcabro@example.com>
2019-06-18 19:57:54 +0000

    Modified server.rd to use template. Added template and version up to 0.2.1

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,13 +8,8 @@ powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!(chef style)</h1>
-  <h2> Platform: #{node['platform']} </h2>
-  <h2> Hostname: #{node['hostname']} </h2>
-  <h2> Memory: #{node['memory']['total']} </h2>
-  <h2> CPU MHz: #{node['cpu']['0']['mhz']} </h2>
-  "
+template 'C:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
